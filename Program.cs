0b086deac84374624bfa34e67fbc6644f94cc517
StefanAjax <pianoajax@gmail.com>
2025-04-29 14:26:38 +0200

    Part 1 done

--- Program.cs
+++ Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var lines = File.ReadAllLines("trueData.txt");
+            var jolts = new SortedSet<int>();
+            
+            // Part 1
+            foreach (var line in lines)
+            {
+                jolts.Add(int.Parse(line));
+            }
+            var diffs = new List<int>();
+            int oldjolt = 0;
+            foreach (var jolt in jolts)
+            {
+                int diff = jolt - oldjolt;
+                diffs.Add(diff);
+                oldjolt = jolt;
+            }
+            int numberOfOnes = diffs.Count(x => x == 1);
+            int numberOfThrees = diffs.Count(x => x == 3);
+            int answer = numberOfOnes * (numberOfThrees + 1);
+            Console.WriteLine($"The answer is {answer}.");
+
         }
     }
 }
